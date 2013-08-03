e9bb7e505cb87d3df2c5c9de064c05bc8adcdc6f
robotmayo <robotmayo@gmail.com>
2013-08-03 18:39:13 -0400

    Prettied up the testing

--- js/test.js
+++ js/test.js
@@ -1,3 +1,14 @@
+var output = document.getElementById('output');  
+  
+function assert( outcome, description ) {  
+    var li = document.createElement('li');  
+    li.className = outcome ? 'pass' : 'fail';  
+    li.appendChild( document.createTextNode( description ) );  
+      
+    output.appendChild(li);  
+};
+var log = function(args){console.log(args)}
+
 var stackA = new Stack();
 stackA.add(4);
 stackA.add(1);
@@ -11,6 +22,19 @@ var testArray = ["test 1",4,{F:"F"}];
 var stackC = new Stack(testArray);
 var stackD = new Stack(1,2,3,4,5,6,7,8,9,10);
 
-console.log(stackA.contains(stackB));
-console.log(stackC.contains(testArray[1]));
-console.log(stackD.contains(4));
+assert(stackA.contains(stackB) === true, "StackA contains StackB");
+assert(stackC.contains(testArray[1]) === true, "StackC contains value from array used as arguments");
+assert(stackD.contains(4) === true, "StackD contains the value when inputted as variable argument");
+assert(stackD.contains("POTATO") === false, "StackD should not contain a POTATO");
+
+var listA = new LinkedList();
+listA.push(3);
+
+var listB = new LinkedList(testArray);
+
+
+
+
+assert(listA.get(0).value === 3, "ListA's first value should be 3");
+assert(listA.size() === 1, "ListA should be of size 1");
+assert(listB.size() === testArray.length, "ListB's size should be equal to the array provieded");
